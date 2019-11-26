6080753ff4e54093cca848af33903048dda63745
bjoemar <bacleaanjoemar22@gmail.com>
2019-11-26 13:20:35 +0800

    change the db credentials

--- connection.php
+++ connection.php
@@ -21,12 +21,19 @@
 	// $conn = mysqli_connect($host,$user,$password,$dbname);
 
 
-$host = "mysql://mysql:3306/";
-$user = 'userC78';
-$password = 'iETdYXa0M3wJSiQE';
-$dbname = 'sampledb';
-$conn = mysqli_connect($host,$user,$password,$dbname);
+// $host = "mysql://mysql:3306/";
+// $user = 'userC78';
+// $password = 'iETdYXa0M3wJSiQE';
+// $dbname = 'sampledb';
+// $conn = mysqli_connect($host,$user,$password,$dbname);
 		
-			
+// OPENSHIFT CONNECTION
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("user3CQ");
+$pwd = getenv("3laCQwUPIKxn3IAc");
+$name = getenv("sampledb");
+$connection = mysqli_connect($host,$user,$password,$dbname);
 
  ?>
