3cbceecb0c203c9121fc252b29824cfbee82c7e8
bjoemar <bacleaanjoemar22@gmail.com>
2019-11-26 14:22:49 +0800

    change db

--- connection.php
+++ connection.php
@@ -42,14 +42,14 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($conn->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
     printf("Connected to the database");
 }
-$connection->close();
+$conn->close();
 
 
  ?>
