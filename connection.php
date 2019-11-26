5c9edf8c06e0947e9ea255298af5c2010729f4d2
bjoemar <bacleaanjoemar22@gmail.com>
2019-11-26 12:57:14 +0800

    change the db port

--- connection.php
+++ connection.php
@@ -14,11 +14,19 @@
 	// $conn = mysqli_connect($host,$user,$password,$dbname);
 		
 
-	$host = "localhost";
-	$user = 'cakephp';
-	$password = 'joemar12';
-	$dbname = 'kor';
-	$conn = mysqli_connect($host,$user,$password,$dbname);
+	// $host = "localhost";
+	// $user = 'cakephp';
+	// $password = 'joemar12';
+	// $dbname = 'kor';
+	// $conn = mysqli_connect($host,$user,$password,$dbname);
+
+
+$host = "mysql://mysql:3306/";
+$user = 'userC78';
+$password = 'iETdYXa0M3wJSiQE';
+$dbname = 'sampledb';
+$conn = mysqli_connect($host,$user,$password,$dbname);
+		
 			
 
  ?>
